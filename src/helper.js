02cc8d1cdaace881f41cf9b2967bdb5563804083
Tom King <design.tomking@gmail.com>
2018-08-13 15:49:01 -0600

    Pass iteration zero tests

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,15 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.stats = this.removeDuplicates(data);
+  }
 
+  removeDuplicates = (data) => {
+    return data.reduce((uniqueObj, school) => {
+      if (!uniqueObj[school.Location]) {
+        uniqueObj[school.Location] = [];
+      }
+      uniqueObj[school.Location].push(school);
+      return uniqueObj;
+    }, {});
+  }
 }
