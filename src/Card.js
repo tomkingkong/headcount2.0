67a4dbc2488a54ce3d034c3c68011ef2911536cb
Tom King <design.tomking@gmail.com>
2018-08-13 22:57:52 -0600

    Create card and cardlist tests

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Card = ({Date, Location}) => {
   const dates = Object.keys(Date).map(year => <li key={Location+year}>{year}: {Date[year]}</li>)
+  
   return (
     <article>
       <h1>{Location}</h1>
