46fa1a5bcfefa86c1a5c5e45ab469bb2ce94482d
Tom King <design.tomking@gmail.com>
2018-08-13 19:45:02 -0600

    Create cardlist and card components
    Feed mock data to cardlist
    
    Render cards to dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+
+import CardList from './CardList.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      schools: [
+        {'Date': {2005: 0.521, 2017: 0.521, 2018: 0.521, 2019: 0.521 }, 'Location': 'COLORADO', 'id':1},
+        {'Date': {2005: 0.521, 2017: 0.521, 2018: 0.521, 2019: 0.521 }, 'Location': 'COLORADO', 'id':2},
+        {'Date': {2005: 0.521, 2017: 0.521, 2018: 0.521, 2019: 0.521 }, 'Location': 'COLORADO', 'id':3}
+      ]
+    }
+  }
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <CardList cards={this.state.schools}/>
+      </div>
     );
   }
 }
