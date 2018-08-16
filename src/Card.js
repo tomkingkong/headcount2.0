d0dff09357f8318f2726f901073eb5b8d9017f98
Tom King <design.tomking@gmail.com>
2018-08-15 18:55:41 -0600

    Open up list item element for readability
    Pass selected function through props

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './Card.css';
 
-const Card = ({stats, location}) => {
-  const dates = Object.keys(stats).map(year => <li className={stats[year]>0.5 ? 'Green' : 'Red'} key={location+year}>{year}: {stats[year]}</li>)
+const Card = ({stats, location, id, selected}) => {
+  const dates = Object.keys(stats).map(year => (
+    <li 
+      className={stats[year] > 0.5 ? 'Green' : 'Red'} 
+      key={location+year}
+    >
+      {year}: {stats[year]}
+    </li>
+  ))
   
   return (
     <article className="Card">
