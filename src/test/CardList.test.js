0ae709a14a9f9d77ab41cdbe418d77f18833f59e
Tom King <design.tomking@gmail.com>
2018-08-19 10:31:44 -0600

    Add mock data

--- src/test/CardList.test.js
+++ src/test/CardList.test.js
@@ -4,6 +4,39 @@ import { shallow, mount } from 'enzyme';
 import Card from '../Card';
 import CardList from '../CardList.js';
 
+const mockData = [
+  {
+    'stats': {
+      2005: 0.521, 
+      2017: 0.521, 
+      2018: 0.521, 
+      2019: 0.521 
+    }, 
+    'location': 'COLORADO', 
+    'id':'COLORADO'
+  },
+  {
+    'stats': {
+      2005: 0.521, 
+      2017: 0.521, 
+      2018: 0.521, 
+      2019: 0.521 
+    }, 
+    'location': 'ASPEN', 
+    'id':'ASPEN'
+  },
+  {
+    'stats': {
+      2005: 0.521, 
+      2017: 0.521, 
+      2018: 0.521, 
+      2019: 0.521 
+    }, 
+    'location': 'SHMOOP', 
+    'id':'SHMOOP'
+  }
+];
+
 describe('CardList', () => {
   let wrapper;
   let mockData = [
