9801f30db24613971b99ed047ea88c0a4b0096b2
Tom King <design.tomking@gmail.com>
2018-08-19 11:09:54 -0600

    Add card component

--- src/Card.js
+++ src/Card.js
@@ -12,17 +12,17 @@ const Card = ({
 }) => {
 
   const displayCardData = () => {
-  const dates = Object.keys(stats).map(year => (
+    const dates = Object.keys(stats).map(year => (
       <li className={stats[year] > 0.5 ? 'Green' : 'Red'} 
-      key={location+year}
-    >
-      {year}: {stats[year]}
-    </li>
+        key={location+year}
+      >
+        {year}: {stats[year]}
+      </li>
     ));
-  return (
+    return (
       <div>
-      <h1>{location}</h1>
-      <ul>{dates}</ul>
+        <h1>{location}</h1>
+        <ul>{dates}</ul>
       </div>
     );
   };
