b7df54b2236c5154074d7d74b6697839f89a26bf
Tom King <design.tomking@gmail.com>
2018-08-19 10:32:11 -0600

    Remove before each and old mock data

--- src/test/CardList.test.js
+++ src/test/CardList.test.js
@@ -38,12 +38,6 @@ const mockData = [
 ];
 
 describe('CardList', () => {
-  let wrapper;
-  let mockData = [
-    {'Date': {2005: 0.521, 2017: 0.521, 2018: 0.521, 2019: 0.521 }, 'Location': 'COLORADO', 'id':1},
-    {'Date': {2005: 0.521, 2017: 0.521, 2018: 0.521, 2019: 0.521 }, 'Location': 'COLORADO', 'id':2},
-    {'Date': {2005: 0.521, 2017: 0.521, 2018: 0.521, 2019: 0.521 }, 'Location': 'COLORADO', 'id':3}
-  ]
 
   beforeEach(() => {
     wrapper = shallow(<CardList cards={mockData} />);
