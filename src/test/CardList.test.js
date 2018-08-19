761002a1b08540871093806e5e92d8414798597b
Tom King <design.tomking@gmail.com>
2018-08-19 10:31:33 -0600

    Remove react dom from import and import card component

--- src/test/CardList.test.js
+++ src/test/CardList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 
+import Card from '../Card';
 import CardList from '../CardList.js';
 
 describe('CardList', () => {
