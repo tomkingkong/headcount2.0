85aa954f6fc6d1b6a0c17c207f1e7b0097d9bdf3
Tom King <design.tomking@gmail.com>
2018-08-19 22:25:05 -0600

    Create comparecard tests
    Check for render with and without props

---
+++ src/test/CompareCard.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import CompareCard from '../CompareCard';
+
+const mockData =  {
+  "comparedData": {
+    'COLORADO': 0.53, 
+    'ASPEN 1': 0.999,
+    'compared': 0.531
+  }
+};
+
+describe('CompareCard', () => {
+
+  it('should match the snapshot passing an empty data object', () => {
+    const wrapper = shallow(<CompareCard comparedData={{}}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot passing an array with data', () => {
+    const wrapper = shallow(<CompareCard comparedData={mockData}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
