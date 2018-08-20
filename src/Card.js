7f8dff947e2174b635bb2fc603dec9ed83f5d2a6
Tom King <design.tomking@gmail.com>
2018-08-19 20:37:48 -0600

    Create render method

--- src/Card.js
+++ src/Card.js
@@ -20,39 +20,33 @@ class Card extends Component {
       })
     }
   }
+  
+  render() {
+    const { stats, location, compare } = this.props;
+    let dates;
+    if (stats) {
+      dates = Object.keys(stats).map(year => (
+        <li 
+          className={stats[year] > 0.5 ? "Green" : "Red"} 
         key={location+year}
       >
         {year}: {stats[year]}
       </li>
     ));
+    }
     return (
+      <article 
+        className={this.state.selected || compare ? "Card selected" : "Card"} 
+        onClick={this.handleClick} 
+      >
       <div>
         <h1>{location}</h1>
         <ul>{dates}</ul>
       </div>
-    );
-  };
-
-  const displayCompareData = () => {
-    const data = Object.keys(comparedData);
-    return (
-      <div>
-        <h2>{data[0]}: {comparedData[data[0]]}</h2>
-        <p>{comparedData.compared}</p>
-        <h2>{data[1]}: {comparedData[data[1]]}</h2>
-      </div>
-    );
-  };
-  
-  return (
-    <article className="Card" 
-      onClick={() => { toggleCompare && toggleCompare(id); }} 
-    >
-      {comparedData && displayCompareData()}
-      {stats && displayCardData()}
     </article>
   );
 };
+}
 
 Card.propTypes = {
   stats: PropTypes.object, 
