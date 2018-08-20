26e9d9537452c3458763df4a5f71a152a7ee85d0
Tom King <design.tomking@gmail.com>
2018-08-19 20:33:44 -0600

    Rename school to data in all instances

--- src/test/Card.test.js
+++ src/test/Card.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Card from '../Card';
-const school = {
+const data = {
   'stats': { 
     2005: 0.521, 
     2017: 0.521, 
@@ -16,36 +16,36 @@ const school = {
 describe('Card', () => {
     
   it('should match snapshot with a data object', () => {
-    const wrapper = shallow(<Card {...school} key={school.id}/>);
+    const wrapper = shallow(<Card {...data} key={data.id}/>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should match snapshot with a data object and function', () => {
-    const selectSchool = jest.fn();
-    const wrapper = shallow(<Card {...school} key={school.id} selected={selectSchool}/>);
+    const toggleCompare = jest.fn();
+    const wrapper = shallow(<Card {...data} key={data.id} selected={toggleCompare}/>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should match snapshot with an empty data object', () => {
-    const schoolEmpty = {'stats': { }, 'location': '', 'id':''};
-    const wrapper = shallow(<Card {...schoolEmpty} key={schoolEmpty.id}/>);
+    const dataEmpty = {'stats': { }, 'location': '', 'id':''};
+    const wrapper = shallow(<Card {...dataEmpty} key={dataEmpty.id}/>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should render the correct number of li\'s', () => {
-    const wrapper = shallow(<Card {...school} key={school.id}/>);
+    const wrapper = shallow(<Card {...data} key={data.id}/>);
     expect(wrapper.find('li').length).toEqual(4);
   });
 
   it('should add Green class to li if percent is greater than 0.5', () => {
-    const schoolEmpty = {'stats': {2001: 0.6}, 'location': '', 'id':''};
-    const wrapper = shallow(<Card {...schoolEmpty} key={schoolEmpty.id}/>);
+    const dataEmpty = {'stats': {2001: 0.6}, 'location': '', 'id':''};
+    const wrapper = shallow(<Card {...dataEmpty} key={dataEmpty.id}/>);
     expect(wrapper.find('li').hasClass('Green')).toEqual(true);
   });
 
   it('should add Red class to li if percent is lesser than 0.5', () => {
-    const schoolEmpty = {'stats': { 2001: 0.4}, 'location': '', 'id':''};
-    const wrapper = shallow(<Card {...schoolEmpty} key={schoolEmpty.id}/>);
+    const dataEmpty = {'stats': { 2001: 0.4}, 'location': '', 'id':''};
+    const wrapper = shallow(<Card {...dataEmpty} key={dataEmpty.id}/>);
     expect(wrapper.find('li').hasClass('Red')).toEqual(true);
   });
 
